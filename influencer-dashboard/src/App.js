8f0736bd1f53ac0e656e2a0c5225ad6858c1b50d
Ibrahim Qureshi <ibrahim.qureshi@sapphire.co>
2026-04-14 19:31:03 +0500

    Fix: disable ESLint rule globally for useEffect deps

--- influencer-dashboard/src/App.js
+++ influencer-dashboard/src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
@@ -39,7 +40,6 @@ function App() {
   });
 
   // Load data from backend
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
     fetchCampaigns();
     fetchStats();
